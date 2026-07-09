aa0706b23ff2cd0b45da76d4841fc2a8dac55614
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-07-09 22:14:24 +0200

    Remove validation

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -10,21 +10,14 @@ Handle missing values:
 def clean_total_charges(df, method='drop'):
     """Handles missings in total_charges field"""
 
-    # Prepare TotalCharges 
-
-    # Error messages
-    mver = 'Method shall be: "drop", "median", or "impute"'
-
     if method == 'drop':
         df = df.dropna(subset=['TotalCharges'])
-    elif method == 'median':
+    if method == 'median':
         df['TotalCharges'] = df.to_numeric(df['TotalCharges'], errors='coerce')
         median_value = df['TotalCharges'].median()
         df['TotalCharges'] = df['TotalCharges'].fillna(median_value)
-    elif method == 'impute':
+    if method == 'impute':
         impute_charge = df['MonthlyCharges'] * df['tenure']
         df['TotalCharges'] = df['TotalCharges'].fillna(impute_charge)
-    else:
-        raise ValueError(mver)
 
     return df
