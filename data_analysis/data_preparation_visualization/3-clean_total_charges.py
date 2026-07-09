01089a65c6982692de4d77678257b620bac1cc6c
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-07-09 22:18:33 +0200

    PD.to_numeric, not df.to_numeric

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -13,7 +13,7 @@ def clean_total_charges(df, method='drop'):
     if method == 'drop':
         df = df.dropna(subset=['TotalCharges'])
     if method == 'median':
-        df['TotalCharges'] = df.to_numeric(df['TotalCharges'], errors='coerce')
+        df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
         median_value = df['TotalCharges'].median()
         df['TotalCharges'] = df['TotalCharges'].fillna(median_value)
     if method == 'impute':
