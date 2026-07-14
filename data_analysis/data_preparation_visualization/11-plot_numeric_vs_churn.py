c7ab6a0a293f614ddf56acd07134ef2f9d963a7a
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-07-14 14:43:53 +0200

    Style

--- data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
+++ data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
@@ -15,9 +15,7 @@ def plot_numeric_vs_churn(df, col):
     sit_puts = df[df['Churn'] == 'No'][col]
     fuc_offs = df[df['Churn'] == 'Yes'][col]
 
-    plt.hist([sit_puts, fuc_offs],
-              bins=30,
-              label=['No', 'Yes'])
+    plt.hist([sit_puts, fuc_offs], bins=30, label=['No', 'Yes'])
 
     plt.title(f'{col} Distribution by Churn')
     plt.xlabel(f'{col}')
