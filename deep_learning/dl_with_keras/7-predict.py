483ddceb73c8721776b3f353833a5d97fddb9400
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-08-07 18:22:10 +0200

    make trained model predict'

---
+++ deep_learning/dl_with_keras/7-predict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Generate predictions using a trained model
+"""
+import tensorflow as tf
+
+
+def predict(model, X, verbose=0):
+    """
+    Model, act my dear!
+    Predict class labels for input data X
+    """
+
+    probabilities = model.predict(X, verbose=verbose)
+    predicted_classes = tf.argmax(
+        probabilities,
+        axis=1
+    )
+    return predicted_classes.numpy().tolist()
