b16e6c08d1250ea68cce4169452fd880a71e0b4c
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-08-09 14:28:48 +0200

    Return tf argmax output

--- deep_learning/dl_with_keras/7-predict.py
+++ deep_learning/dl_with_keras/7-predict.py
@@ -7,11 +7,8 @@ import tensorflow as tf
 
 def predict(model, X, verbose=0):
     """
-    Model, act my dear!
     Predict class labels for input data X
     """
-
     probabilities = model.predict(X, verbose=verbose)
-    predicted_classes = tf.math.argmax(probabilities, axis=1)
 
-    return predicted_classes.numpy()
+    return tf.argmax(probabilities, axis=1)
