fdb09b3e87fbdc5fbcf4646d7b2396abb96643be
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-08-26 21:47:07 +0200

    Try 0 without import keras and with test reqs

--- deep_learning/transfer_learning/0-frozen_extractor.py
+++ deep_learning/transfer_learning/0-frozen_extractor.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
+"""
+Pull a awift brain and preserve it's knowledge
+"""
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+
+def build_feature_extractor():
+    """
+    Loads MobileNetV2 without top layer,
+    freezes its weights, and
+    attaches a globalAveragePooling2D layer on top.
+    """
+
+    base_model = keras.applications.MobileNetV2(
+        weights='imagenet',
+        include_top=False,
+        input_shape=(224, 224, 3)
+    )
+    base_model.trainable = False
+
+    model = keras.Sequential([
+        base_model,
+        keras.layers.GlobalAveragePooling2D()
+    ])
+
+    return model
