3babb15a5ab40f78608a338dbddb886f4625c1eb
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-08-26 21:57:49 +0200

    Specify input layer

--- deep_learning/transfer_learning/0-frozen_extractor.py
+++ deep_learning/transfer_learning/0-frozen_extractor.py
@@ -4,13 +4,13 @@ Pull a awift brain and preserve it's knowledge
 """
 from tensorflow import keras
 
+
 def build_feature_extractor():
     """
     Loads MobileNetV2 without top layer,
     freezes its weights, and
-    attaches a globalAveragePooling2D layer on top.
+    attaches a GlobalAveragePooling2D layer on top.
     """
-
     base_model = keras.applications.MobileNetV2(
         weights='imagenet',
         include_top=False,
@@ -19,6 +19,7 @@ def build_feature_extractor():
     base_model.trainable = False
 
     model = keras.Sequential([
+        keras.layers.Input(shape=(224, 224, 3)),
         base_model,
         keras.layers.GlobalAveragePooling2D()
     ])
