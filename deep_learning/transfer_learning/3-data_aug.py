00bcafa49c1023e714bfe1cde08dc63f75414fab
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-08-26 23:13:44 +0200

    change perspective with augmentation

--- deep_learning/transfer_learning/3-data_aug.py
+++ deep_learning/transfer_learning/3-data_aug.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python3
+"""
+Literaly build a reusable augmentation pipe. Pip-pip!
+"""
+import tensorflow as tf
 
+
+def build_data_augmentation():
+    """
+    Return a seeded Sequential image augumentation model
+    """
+
+    seed = 42
+    return tf.keras.Sequential([
+        tf.keras.layers.RandomFlip(
+            'horizontal',
+            seed
+        ),
+        tf.keras.layers.RandomRotation(
+            0.15,
+            seed
+        ),
+        tf.keras.layers.RandomZoom(
+            0.15,
+            seed
+        ),
+        tf.keras.layers.RandomContrast(
+            0.1,
+            seed
+        )
+    ])
