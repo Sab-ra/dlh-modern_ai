7312bfb634e9a26c131ce0b80b071979f0d8ae57
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-09-16 15:05:50 +0200

    train model

--- deep_learning/cv_apps/3-train_aug.py
+++ deep_learning/cv_apps/3-train_aug.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+"""
+Train YOLOv8 with optional custom Albumentations
+"""
+from ultralytics import YOLO
+
+
+def train_with_augmentation(
+        data,
+        model_path='yolov8n.pt',
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        augmentation=True,
+        yolo_aug_params=None,
+        albumentations_transforms=None,
+        save=True,
+        plots=True,
+        verbose=True
+):
+    """
+    Train a YOLO model with native and
+    custom augmentation
+    """
+    model = YOLO(model_path)
+    train_kw = {
+        'data': data,
+        'epochs': epochs,
+        'imgsz': imgsz,
+        'batch': batch,
+        'augment': augmentation,
+        'save': save,
+        'plots': plots,
+        'verbose': verbose
+    }
+    if yolo_aug_params:
+        train_kw.update(yolo_aug_params)
+    if albumentations_transforms:
+        train_kw['augmentations'] = (
+            albumentations_transforms
+        )
+    results = model.train(**train_kw)
+
+    return model, results
