491a91028aade4addeab797880bf1c507c150ee3
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-09-16 14:36:41 +0200

    do custom augmentation

--- deep_learning/cv_apps/2-custom_aug.py
+++ deep_learning/cv_apps/2-custom_aug.py
@@ -1,1 +1,46 @@
 #!/usr/bin/env python3
+"""
+YOLO-compatible custom augmentation
+with Albumentations
+"""
+import albumentations as A
+import numpy as np
+
+
+def custom_aug(image, bboxes, labels):
+    """
+    Apply motion blur and one geomentric warp
+    to an image and its Pascal VOC boxes
+    """
+    transform = A.Compose(
+        [
+            A.MotionBlur(blur_limit=5, p=0.9),
+            A.OneOf(
+                [
+                    A.ElasticTransform(
+                        alpha=1,
+                        sigma=50,
+                        p=0.2
+                    ),
+                    A.OpticalDistortion(
+                        distort_limit=0.05,
+                        p=0.2
+                    )
+                ],
+                bbox_params=A.BboxParams(
+                    format='pascal_voc',
+                    label_fields=['labels']
+                ),
+                seed=42
+            )
+        ]
+    )
+    output = transform(
+        image = image,
+        bboxes=bboxes,
+        labels=labels
+    )
+    boxes = np.asarray(output['bboxes'])
+    labs = [int(x) for x in output['labels']]
+
+    return output['image'], boxes, labs
