64e41781a7945ef8ab2cde8b36a748135d8ed8db
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-09-17 19:31:19 +0200

    legthT

--- data_analysis/intro_to_nlp/0-explore_data.py
+++ data_analysis/intro_to_nlp/0-explore_data.py
@@ -32,7 +32,7 @@ def explore_data(df):
         ax=ax2
     )
     ax2.set_title('Histogram of Raw Message Lengths')
-    ax2.set_xlabel('lenght')
+    ax2.set_xlabel('lengh')
     ax2.set_ylabel('count')
 
     plt.tight_layout()
