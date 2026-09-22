87125f1b6c540659853d8a1e24642808b8478396
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-09-22 13:32:17 +0200

    Remove stop-words from tokens

--- data_analysis/intro_to_nlp/3-remove_stopwords.py
+++ data_analysis/intro_to_nlp/3-remove_stopwords.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""
+Stopwords removal for tokenized text messages.
+"""
+import nltk
+
+
+def remove_stopwords(
+        tokens,
+        language='english',
+        extra_words=None,
+        keep_words=None
+):
+    """
+    Filter stopwords out of a token list.
+    """
+    if not isinstance(tokens, list):
+        return []
+
+    stop_words = set(nltk.corpus.stopwords.words(language))
+
+    if extra_words:
+        stop_words |= set(extra_words)
+    if keep_words:
+        stop_words -= set(keep_words)
+
+    return [t for t in tokens if t.lower() not in stop_words]
